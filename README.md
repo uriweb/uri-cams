3155f8c9c75cc30cb55bebabf8dbfc06a125f25d
John Pennypacker <jpennypacker@uri.edu>
2019-01-03 15:24:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# URI Cams Widget
+
+Add the `[uri-cams]` shortcode to a page and a tides widget appears.
+
+The widget pulls in live water temp and tide prediction data from NOAA and graphically displays the current position of the tide.  By default, data is pulled from the station at Quonset Point, RI, but a different station or buoy can be set if desired.
+
+## Attributes
+
+The tides widget is somewhat configurable by adding attributes to the shortcode:
+
+**`ip`** (num)(optional)  
+The IP address from which to retrieve data. The default is the Bay Campus. (default: `131.128.104.45`)  
+
+**`username`** (num)(optional)  
+Username to access the camera
+
+**`password`** (bool)(optional)  
+Password to access the camera
+
+**`class`** (string)(optional)  
+Set custom CSS class(s) (default: none)
+
+**`alt`** (string)
+The alternative text description of the image from the camera
+
+## Plugin Details
+
+Contributors: Brandon Fuller, John Pennypacker  
+Tags: widgets  
+Requires at least: 4.0  
+Tested up to: 4.9  
+Stable tag: 1.0
---
+++ uri-cams.php
@@ -0,0 +1,144 @@
+<?php
+/*
+Plugin Name: URI Cams
+Plugin URI: https://www.uri.edu
+Description: Webcam picture importer
+Version: 1.0
+Author: URI Web Communications
+Author URI: 
+@author: John Pennypacker <jpennypacker@uri.edu>
+*/
+
+// Block direct requests
+if ( !defined('ABSPATH') )
+	die('-1');
+
+
+
+/**
+ * Loads up the css
+ */
+function uri_cams_styles() {
+	$css_url = plugins_url( '/css/cams.css', __FILE__ );
+	$cache = filemtime( dirname(__FILE__) . '/css/cams.css' );
+	wp_enqueue_style( 'uri-cams-css', $css_url, array(), $cache );
+}
+add_action( 'wp_enqueue_scripts', 'uri_cams_styles' );
+
+
+
+/**
+ * Shortcode callback
+ */
+function uri_cams_shortcode($attributes, $content, $shortcode) {
+
+	// Attributes
+	extract( shortcode_atts(
+		array(
+			'ip' => '131.128.104.45',
+			'username' => 'Viewer',
+			'password' => 'bay campus',
+			'alt' => '',
+			'class' => ''
+		), $attributes )
+	);
+	
+	
+	$transient_name = 'uri_cams_' . $ip;
+
+	if ( false === ( $path = get_site_transient( $transient_name ) ) ) {
+		// It wasn't there, so regenerate the data and save the transient
+		$path = uri_cams_get_image($ip, $username, $password);
+		set_site_transient( $transient_name, $path, 10 * MINUTE_IN_SECONDS );
+	}
+	
+	// @todo what to do if $path isn't set?
+	
+	$file = uri_cams_get_directory() . '/' . uri_cams_get_name($ip);
+	if( file_exists( $file ) ) {
+		$path = uri_cams_get_path() . $file;
+	}
+
+
+
+	$output = '<figure class="uri-cams">';
+	// $output .= strtotime('now');
+	$output .= '<img src="' . $path . '" alt="' . $alt . '" />';
+
+	$output .= '</figure>';
+	return $output;
+
+}
+add_shortcode( 'uri-cams', 'uri_cams_shortcode' );
+
+
+
+
+function uri_cams_get_image( $ip, $username, $password ) {
+
+	$url = 'http://' . $ip . '/media/cam0/still.jpg?res=max';
+	$timeout = 10;
+
+	// set up curl
+	$ch = curl_init();
+	curl_setopt( $ch, CURLOPT_URL, $url );
+	curl_setopt( $ch, CURLOPT_TIMEOUT, $timeout );
+
+	// return to variable
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+
+	// (don't) verify host ssl cert
+	// curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, $ssl_verifyhost );
+	// (don't) verify peer ssl cert	
+	// curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, $ssl_verifypeer );
+
+	curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
+	// send a user:password
+	curl_setopt( $ch, CURLOPT_USERPWD,  $username . ':' . $password );
+
+	// fetch remote contents
+	if ( false === ( $response = curl_exec( $ch ) ) )	{
+		// if we get an error, use that
+		$error = curl_error( $ch );						
+	}
+	// close the resource
+	curl_close( $ch );
+		
+	if( empty( $error ) ) {
+		$destination = uri_cams_get_directory();
+		$file = uri_cams_save_file( $destination, $response, $ip );
+		$path = uri_cams_get_path() . $file;
+		return $path;
+	}
+	
+}
+
+
+function uri_cams_save_file( $destination, $response, $ip ) {
+	$success = FALSE;
+	if ( wp_mkdir_p( $destination ) ) {
+		$filename =  uri_cams_get_name($ip);
+		$success = file_put_contents(trailingslashit( $destination ) . $filename, $response);
+	}
+	if( $success ) {
+		return $filename;
+	} else {
+		return FALSE;
+	}
+}
+
+
+function uri_cams_get_directory() {
+	return trailingslashit( WP_CONTENT_DIR ) . 'uploads/uri-cams';
+}
+
+function uri_cams_get_path() {
+	return trailingslashit( WP_CONTENT_URL ) . 'uploads/uri-cams/';
+}
+
+function uri_cams_get_name($ip) {
+	return 'uri-cams--' . $ip . '.jpg';
+}
+
+
+
