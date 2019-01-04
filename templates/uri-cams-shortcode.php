9f1ea1069216b97b771efd9c2ccdf233f369b3af
John Pennypacker <jpennypacker@uri.edu>
2019-01-04 16:14:44 -0500

    added code to pull in engineering's images

---
+++ templates/uri-cams-shortcode.php
@@ -0,0 +1,21 @@
+<?php	
+
+	$classes = 'uri-cams';
+	$classes .= ( ! empty( $class ) ) ? ' ' . $class : '';
+	
+?>
+
+<figure class="<?php echo $classes; ?>">
+	
+	<?php if ( false !== $link ): ?>
+		<a href="<?php echo $path . '?t=' . $time; ?>">
+	<?php endif; ?>
+
+	<img src="<?php echo $path . '?t=' . $time; ?>" alt="<?php echo $alt; ?>" title="<?php echo $alt; ?>" />
+
+	<?php if ( false !== $link ): ?>
+		</a>
+	<?php endif; ?>
+
+</figure>
+
